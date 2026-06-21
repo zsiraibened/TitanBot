e5bd1ccc59cfb12e67ceda9d459f24c602e35498
zsiraibened <zsiraiben@gmail.com>
2026-06-21 13:53:32 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Made with skibidi",
         // Activity type number (0 = Playing).
         type: 0,
       },
